8a3fd1ffb7c2065141ea2766a3f114dfb3151eba
Flames01 <98680848+Flames01@users.noreply.github.com>
2022-02-01 09:38:10 +0530

    Add files via upload

---
+++ mathe program.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,b,t;
+   cout <<"prove that : z = at+bt2\n" ;
+  int sum;
+cout << "Type a value of a:\n";
+
+cin >> a;
+    cout << "________________******************________________\n";
+cout << "Type a value of b:\n ";
+cin >> b;
+     cout << "________________******************________________\n";
+
+    cout << "Type a value of t:\n ";
+cin >> t;
+     cout << "________________******************________________\n";
+
+sum = a*t+b*t*t;
+cout << "your answer is:\n z  =  " << sum ;
+     cout << "\n________________******************________________\n";
+ cout << "___________________thankyou___________________\n";
+
+
+}
